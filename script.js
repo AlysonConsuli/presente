01d228f7e83837bb54fb34c9b953a2627ad1d7cf
Alyson Consuli <consulialyson@gmail.com>
2022-04-10 12:58:56 -0300

    feat: detalhes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let nome = ''
+let token = ''
 const primeiro = document.querySelector('.primeiro')
 const btn = document.querySelector('.entrar')
 const main = document.querySelector('main')
@@ -6,16 +7,22 @@ const main = document.querySelector('main')
 function enter() {
     nome = prompt('Qual o seu nome?')
     if (nome.toLowerCase() === 'alex' || nome.toLowerCase() === 'pai') {
-        alert(`Hoje é seu aniversário ${nome}, Bem vindo ao site!`)
-        primeiro.classList.add('bloqueado')
-        btn.classList.remove('bloqueado')
+        token = prompt(`Prove que vc é o ${nome}. Quem se rebelou contra a UNSC hoje de manhã?`)
+        if (token.toLowerCase() === 'chief' || token.toLowerCase() === 'master chief' || token.toLowerCase() === 'halo') {
+            alert(`Olá ${nome}, Bem vindo ao site!`)
+            primeiro.classList.add('bloqueado')
+            btn.classList.remove('bloqueado')
+        }
+        else{
+            alert('Errado!')
+        }
     }
     else {
         alert('Hoje não é seu aniversário')
     }
 }
 
-function presente(){
+function presente() {
     btn.classList.add('bloqueado')
     main.classList.remove('bloqueado')
 }
